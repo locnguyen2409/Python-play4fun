02badc5d791f79ee2b1caf94480803a0072d0f45
locnguyen2409 <locngu@metropolia.fi>
2025-08-22 15:49:01 +0300

    change print

---
+++ ex6.py
@@ -0,0 +1,9 @@
+mathematic=float(input("Enter mathematics marks: "))
+physics=float(input("Enter physics marks: "))
+programming=float(input("Enter programming marks: "))
+total=mathematic+physics+programming
+total=float(total)
+percent=float(total/300*100)
+print("total marks: ",total)
+print("percent marks: ",percent,"%")
+
