7b7294338cf80deae80c4daa772f390aafa430dd
locnguyen2409 <locngu@metropolia.fi>
2025-08-29 15:20:33 +0300

    change print

---
+++ 29/8.py
@@ -0,0 +1,7 @@
+first = 2
+while first <= 2:
+    second = 1
+    while second <= 10:
+        print(f"{first} times {second} is {first*second:d}")
+        second = second + 1
+    first = first + 1
