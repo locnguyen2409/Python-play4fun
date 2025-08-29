8412d22f47388fbe8b47d327efd7f182071f73f9
locnguyen2409 <locngu@metropolia.fi>
2025-08-29 14:08:49 +0300

    change print

---
+++ module 3/3.1.py
@@ -0,0 +1,6 @@
+length = float(input("What is the length of a zendar in centimeters?: "))
+if length > 42:
+    print("The length of the zendar meets the size limit.")
+else:
+    print("You have to release the fish back into the lake.")
+print("Goodbye!")
