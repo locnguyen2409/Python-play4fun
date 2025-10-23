75f0d9b0503b6706c16cf0f7314658cc2c227231
locnguyen2409 <locngu@metropolia.fi>
2025-10-23 18:28:25 +0300

    change print

--- module 9/9.4.py
+++ module 9/9.4.py
@@ -1,4 +1,6 @@
 import random
+
+
 class Car:
     def __init__(self, registration_number, max_speed):
         self.registration_number = registration_number
@@ -15,24 +17,28 @@ class Car:
 
     def drive(self, hours):
         self.travelled_distance += self.current_speed * hours
-cars = []
-for i in range (1,11):
-    registration_number = f"ABC-{i}"
-    max_speed = random.randint(100,200)
-    cars.append(Car(registration_number, max_speed))
-race_over=0
-hour=0
-while race_over:
-    hour += 1
+
+if __name__ == "__main__":
+    print("Connection Successful")
+
+    cars = []
+    for i in range (1,11):
+        registration_number = f"ABC-{i}"
+        max_speed = random.randint(100,200)
+        cars.append(Car(registration_number, max_speed))
+    race_over=True
+    hour=0
+    while race_over:
+        hour += 1
+        for car in cars:
+            speed_change = random.randint(-10,15)
+            car.accelerate(speed_change)
+            car.drive(1)
+            if car.travelled_distance > 10000:
+                race_over = False
+                break
+    print(f"\n Race finished in {hour} hours!\n")
+    print(f"{'Car':<10}{'Max Speed':<12}{'Current Speed':<15}{'Distance Travelled (km)':<25}")
+    print("-" * 65)
     for car in cars:
-        speed_change = random.randint(-10,15)
-        car.accelerate(speed_change)
-        car.drive(1)
-        if car.travelled_distance > 10000:
-            race_over = True
-            break
-print(f"\n Race finished in {hour} hours!\n")
-print(f"{'Car':<10}{'Max Speed':<12}{'Current Speed':<15}{'Distance Travelled (km)':<25}")
-print("-" * 65)
-for car in cars:
-    print(f"{car.registration_number:<10}{car.max_speed:<12}{car.current_speed:<15}{car.travelled_distance:<25.1f}")
+        print(f"{car.registration_number:<10}{car.max_speed:<12}{car.current_speed:<15}{car.travelled_distance:<25.1f}")
